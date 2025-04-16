052ae54648a83862bc563a6c1e1863afb2a9a511
AssassinMaeve <maeve.fernandes.7@gmail.com>
2025-04-16 12:35:25 +0530

    Service Music Player

--- Service/app/src/main/java/com/example/myapplication/Service/MyMusicService.java
+++ Service/app/src/main/java/com/example/myapplication/Service/MyMusicService.java
@@ -2,10 +2,28 @@ package com.example.myapplication.Service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.IBinder;
 
+import com.example.myapplication.R;
+
 public class MyMusicService extends Service {
+
+    MediaPlayer mp;
     public MyMusicService() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mp = MediaPlayer.create(this, R.raw.ineedmoreofyou);
+        mp.start();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mp.stop();
     }
 
     @Override
