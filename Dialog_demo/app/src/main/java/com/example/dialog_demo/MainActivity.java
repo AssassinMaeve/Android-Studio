e91122a06164f8361ded2c88ee644661e198401e
AssassinMaeve <maeve.fernandes.7@gmail.com>
2025-04-16 12:54:35 +0530

    Dialog Alarm Program in progress

---
+++ Dialog_demo/app/src/main/java/com/example/dialog_demo/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.dialog_demo;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText datetxt, timetxt;
+    Button aadbtn;
+    Calendar cal;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
