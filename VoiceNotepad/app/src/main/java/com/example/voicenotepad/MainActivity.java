2257146577e16b1270ac6eb7f74c20c1dc4d9deb
AssassinMaeve <maeve.fernandes.7@gmail.com>
2025-05-10 12:21:29 +0530

    Voice Notepad

---
+++ VoiceNotepad/app/src/main/java/com/example/voicenotepad/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.voicenotepad;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText txtContent;
+    Button buttonConvert;
+    ImageButton btnSpeak;
+    TextView txtChars;
+    protected static final int RESULT_SPEECH = 1;
+    TextToSpeech tts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        txtContent = findViewById(R.id.txtContent);
+        buttonConvert = findViewById(R.id.button);
+        btnSpeak = findViewById(R.id.imageButton);
+        txtChars = findViewById(R.id.txtChars);
+
+        buttonConvert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+                    @Override
+                    public void onInit(int status) {
+                        if(status == TextToSpeech.SUCCESS){
+                            tts.setLanguage(Locale.US);
+                            tts.setSpeechRate(1.0f);
+                            tts.speak(txtContent.getText().toString(),TextToSpeech.QUEUE_ADD,null);
+                        }
+                    }
+                });
+            }
+        });
+
+        btnSpeak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,"en-US");
+
+                try{
+                    startActivityForResult(intent, RESULT_SPEECH);
+                    txtChars.setText("");
+                } catch(ActivityNotFoundException e){
+                    Toast.makeText(MainActivity.this, "Your Device "+ "does not support speech recognition", Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                }
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode){
+            case RESULT_SPEECH:
+                if(resultCode == RESULT_OK && data != null){
+                    ArrayList<String> text = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    txtChars.setText(text.get(0));
+                }
+                break;
+        }
+    }
+}
